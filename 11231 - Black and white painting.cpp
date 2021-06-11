3b356466f818784f3b3d48f93df401ff98170775
IFTEKHAR AHAMED SIDDIQUEE <70750068+Iftekhar-Ahamed@users.noreply.github.com>
2021-06-12 00:19:31 +0600

    Create 11231 - Black and white painting.cpp

---
+++ 11231 - Black and white painting.cpp
@@ -0,0 +1,66 @@
+/***
+**      Bismillahir Rahmanir Rahim
+**              ALLAHU AKBAR
+**
+**     Author: Iftekhar Ahamed Siddiquee
+**     Bangladesh University of Business and Technology,
+**     Dept. of CSE.
+***/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+using namespace std;
+
+#define read freopen("input.txt","r",stdin)
+#define write freopen("output.txt","w",stdout)
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int uint64;
+#define lld long long int
+#define INF (int)1e9
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+#define mXs 1000000
+const double pi = acos(-1.0);
+
+
+int main()
+{
+    cin.tie(NULL);
+    //read;
+    //write;
+    ios_base::sync_with_stdio(false);
+    lld n,m,c;
+    while (cin>>n>>m>>c)
+    {
+        if(n+m+c==0)break;
+        if(c==1){
+            cout<<((n-7)*(m-7)+1)/2<<endl;
+        }else{
+            cout<<((n-7)*(m-7))/2<<endl;
+        }
+    }
+    
+    return 0;
+}
