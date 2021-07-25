4fea1db02776068fae338894ea1b14ddfbd53a79
IFTEKHAR AHAMED SIDDIQUEE <70750068+Iftekhar-Ahamed@users.noreply.github.com>
2021-07-25 11:11:21 +0600

    Create 11683 - Laser Sculpture.cpp

---
+++ 11683 - Laser Sculpture.cpp
@@ -0,0 +1,72 @@
+/***
+**      Bismillahir Rahmanir Rahim
+**              ALLAHU AKBAR
+**
+**     Author: Iftekhar Ahamed Siddiquee
+**     Bangladesh University of Business and Technology,
+**     Dept. of CSE.
+***/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+using namespace std;
+
+#define read freopen("input.txt","r",stdin)
+#define write freopen("output.txt","w",stdout)
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int uint64;
+#define lld long long int
+#define INF (int)1e9
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+const int mXs = 1e7;
+const double pi = acos(-1.0);
+
+
+int main()
+{
+    cin.tie(NULL);
+    //read;
+    //write;
+    ios_base::sync_with_stdio(false);
+    lld n,m,ans,i,x,pev;
+    while (cin>>n && n!=0)
+    {
+        cin>>m;
+        cin>>x;
+        ans=n-x;
+        pev=x;
+        for(i=1;i<m;i++){
+            cin>>x;
+            if(pev>x){
+                ans+=(pev-x);
+            }
+            pev=x;
+        }
+        cout<<ans<<endl;
+    }
+    
+    return 0;
+}
