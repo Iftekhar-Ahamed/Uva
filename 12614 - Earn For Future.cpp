17d1aaef94a68038f92f8f4df2afb7daf58ed272
IFTEKHAR AHAMED SIDDIQUEE <70750068+Iftekhar-Ahamed@users.noreply.github.com>
2021-07-25 10:31:10 +0600

    Create 12614 - Earn For Future.cpp

---
+++ 12614 - Earn For Future.cpp
@@ -0,0 +1,68 @@
+/***
+**      Bismillahir Rahmanir Rahim
+**              ALLAHU AKBAR
+**
+**     Author: Iftekhar Ahamed Siddiquee
+**     Bangladesh University of Business and Technology,
+**     Dept. of CSE.
+***/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+using namespace std;
+
+#define read freopen("input.txt","r",stdin)
+#define write freopen("output.txt","w",stdout)
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int uint64;
+#define lld long long int
+#define INF (int)1e9
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+const int mXs = 1e7;
+const double pi = acos(-1.0);
+
+
+int main()
+{
+    cin.tie(NULL);
+    //read;
+    //write;
+    ios_base::sync_with_stdio(false);
+    int t,n,x,m=-1,count=0;
+    cin>>t;
+    while (t--)
+    {
+        cin>>n;
+        m=-1;
+        while(n--){
+            cin>>x;
+            m=max(m,x);
+        }
+        cout<<"Case "<<++count<<": "<<m<<endl;
+    }
+    
+    return 0;
+}
